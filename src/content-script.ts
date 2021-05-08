d8ce6b189f14bbfe2a8a3327e429855530f876b0
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-07 17:32:00 -0700

    Feature: working fetch for play items

--- src/content-script.ts
+++ src/content-script.ts
@@ -23,46 +23,89 @@ function init() {
                 display: none;
                 border-radius: 3px;
             }
+            #tm-code {
+                max-width: 1px;
+                opacity: 0;
+            }
             #tm-info.copied {
                 background-color: green;
                 color: white;
                 display: block;
+            }
+            #tm-info.failed {
+                background-color: red;
+                color: white;
+                display: block;
+                min-width: 10rem;
             }
         </style>
 
         <div id="tm-root">
             <div>
                 <button>O</button>
-                <input id="tm-code" style="max-width: 1px; opacity: 0" spellcheck="false" value="some code">
+                <input id="tm-code" spellcheck="false">
             </div>
-
-            <div id="tm-info">
-                copied
-            </div>
+            <div id="tm-info"></div>
         </div>
     `;
+
     let btn = container.querySelector('button');
     let code = container.querySelector('#tm-code') as HTMLInputElement;
     let info = container.querySelector('#tm-info') as HTMLElement;
+
+    function showResult(ok: boolean) {
+        let className = ok ? 'copied' : 'failed';
+        let delay = ok ? 500 : 1500;
+        info.innerText = ok ? 'copied' : 'failed. check console.';
+        info.classList.add(className);
+        setTimeout(() => info.classList.remove(className), delay);
+    }
 
     function copyToClipboard(e: MouseEvent) {
         const el = code;
         el.select();
         el.setSelectionRange(0, 9999999);
         document.execCommand('copy');
-        info.classList.add('copied');
-    
-        setTimeout(() => {
-            info.classList.remove('copied');
-        }, 500);
+        showResult(true);
     }
 
-    function collectData() {
-        //let
+    async function collectData() {
+        let html = document.documentElement.outerHTML;
+
+        function getPlayerItemsUrl(html: string): string {
+            const reAxiosItemsQuery = /\/course\/\d{3,10}?\/lessons/g;
+            let m: RegExpExecArray | null = reAxiosItemsQuery.exec(html);
+            return m ? `https://coursehunter.net${m[0]}` : '';
+        }
+
+        let itemsUrl = getPlayerItemsUrl(html); //https://coursehunter.net/course/208/lessons"
+        if (!itemsUrl) {
+            throw new Error("Cannot find play items link");
+        }
+        
+        //itemsUrl='';
+        let res = await fetch(itemsUrl);
+        let items = res.ok && await res.json();
+        if (!items) {
+            throw new Error("Cannot fetch play items");
+        }
+
+        let source = {
+            doc: html,
+            items: items,
+        }
+
+        code.value = JSON.stringify(source);
     }
-    
-    btn?.addEventListener('click', (e) => {
-        copyToClipboard(e);
+
+    btn?.addEventListener('click', async (e) => {
+        try {
+            await collectData();
+            copyToClipboard(e);
+        } catch (error) {
+            showResult(false);
+            console.log('tm error', error);
+        }
     }, false);
 
     console.log(btn, code, info);
