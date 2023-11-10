9f737291301a33fc2e389f5edc4d21dc72c642ef
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-10 13:57:12 -0800

    WIP. made main fuctions pure

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,81 +1,9 @@
 function main() {
-    let container = document.createElement('div');
-    container.style.position = 'absolute';
-    container.style.left = '0';
-    container.style.top = '0';
-    container.style.zIndex = '1000';
 
-    container.innerHTML = `
-        <style>
-            #tm-root > * {
-                margin: 0;
-                font-family: Avenir, Helvetica, Arial, sans-serif;
-                font-size: .6rem;
-            }
-            #tm-root button {
-                border: 1px solid #272727;
-            }
-            #tm-btns {
-                display: none;
-                margin-left: .2rem;
-            }
-            #tm-code {
-                /*max-width: 1px; opacity: 0;*/
-                display: none;
-                font-size: .5rem;
-                margin-left: .2rem;
-            }
-            #tm-info {
-                position: absolute;
-                top: 1.4rem;
-                left: 1.2rem;
-                font-size: .9rem;
-                padding: .2rem .4rem;
-                display: none;
-                border-radius: 3px;
-            }
-            #tm-info.copied {
-                background-color: green;
-                color: white;
-                display: block;
-            }
-            #tm-info.failed {
-                background-color: red;
-                color: white;
-                display: block;
-                min-width: 10rem;
-            }
-        </style>
-
-        <div id="tm-root">
-            <div style="display: flex">
-                <button id="tm-run">o</button>
-
-                <div id="tm-btns">
-                    <button>copy</button>
-                    <button style="margin-left: -1px">x</button>
-                </div>
-
-                <input id="tm-code" spellcheck="false">
-            </div>
-            <div id="tm-info"></div>
-        </div>
-    `;
-
-    let _btn = container.querySelector<HTMLButtonElement>('#tm-run')!;
-    let _btns = container.querySelector<HTMLElement>('#tm-btns')!;
-    let [_btncopy, _btnclose] = _btns?.querySelectorAll('button');
-    let _code = container.querySelector<HTMLInputElement>('#tm-code')!;
-    let _info = container.querySelector<HTMLElement>('#tm-info')!;
-
-    let ui = {
-        btn: _btn,
-        btns: _btns,
-        btncopy: _btncopy,
-        btnclose: _btnclose,
-        code: _code,
-        info: _info,
-    };
+    let container = createContainer();
+    let ui = initUi(container);
+    addListeners(ui);
+    document.body.insertBefore(container, document.body.firstElementChild);
 
     async function collectData() {
         const html = document.documentElement.outerHTML;
@@ -145,36 +73,128 @@ function main() {
         showResult(true);
     }
 
-    ui.btn?.addEventListener('click', async (e) => {
-        try {
+    function createContainer(): HTMLDivElement {
+        let container = document.createElement('div');
+        container.style.position = 'absolute';
+        container.style.left = '0';
+        container.style.top = '0';
+        container.style.zIndex = '1000';
+    
+        container.innerHTML = `
+            <style>
+                #tm-root > * {
+                    margin: 0;
+                    font-family: Avenir, Helvetica, Arial, sans-serif;
+                    font-size: .6rem;
+                }
+                #tm-root button {
+                    border: 1px solid #272727;
+                }
+                #tm-btns {
+                    display: none;
+                    margin-left: .2rem;
+                }
+                #tm-code {
+                    /*max-width: 1px; opacity: 0;*/
+                    display: none;
+                    font-size: .5rem;
+                    margin-left: .2rem;
+                }
+                #tm-info {
+                    position: absolute;
+                    top: 1.4rem;
+                    left: 1.2rem;
+                    font-size: .9rem;
+                    padding: .2rem .4rem;
+                    display: none;
+                    border-radius: 3px;
+                }
+                #tm-info.copied {
+                    background-color: green;
+                    color: white;
+                    display: block;
+                }
+                #tm-info.failed {
+                    background-color: red;
+                    color: white;
+                    display: block;
+                    min-width: 10rem;
+                }
+            </style>
+    
+            <div id="tm-root">
+                <div style="display: flex">
+                    <button id="tm-run">o</button>
+    
+                    <div id="tm-btns">
+                        <button>copy</button>
+                        <button style="margin-left: -1px">x</button>
+                    </div>
+    
+                    <input id="tm-code" spellcheck="false">
+                </div>
+                <div id="tm-info"></div>
+            </div>
+        `;
+        return container;
+    }
+
+    type UI = {
+        btn: HTMLButtonElement;
+        btns: HTMLElement;
+        btncopy: HTMLButtonElement;
+        btnclose: HTMLButtonElement;
+        code: HTMLInputElement;
+        info: HTMLElement;
+    };
+
+    function initUi(container: HTMLElement): UI {
+        let _btns = container.querySelector<HTMLElement>('#tm-btns')!;
+        let [_btncopy, _btnclose] = _btns?.querySelectorAll('button');
+    
+        let ui: UI = {
+            btn: container.querySelector<HTMLButtonElement>('#tm-run')!,
+            btns: _btns,
+            btncopy: _btncopy,
+            btnclose: _btnclose,
+            code: container.querySelector<HTMLInputElement>('#tm-code')!,
+            info: container.querySelector<HTMLElement>('#tm-info')!,
+        };
+        return ui;
+    }
+
+    function addListeners(ui: UI) {
+        ui.btn?.addEventListener('click', async (e) => {
+            try {
+                ui.btns.style.display = 'none';
+                ui.code.style.display = 'none';
+    
+                ui.code.value = '';
+                let collectedData = await collectData();
+                ui.code.value = collectedData;
+    
+                ui.code.style.display = 'block';
+                ui.btns.style.display = 'flex';
+    
+                console.log('tm done'); // leave here for debugging.
+            } catch (error) {
+                ui.btns.style.display = 'none';
+                showResult(false);
+    
+                console.log('tm error', error);
+            }
+        }, false);
+    
+        ui.btncopy?.addEventListener('click', () => {
+            copyToClipboard();
+        }, false);
+    
+        ui.btnclose?.addEventListener('click', () => {
             ui.btns.style.display = 'none';
             ui.code.style.display = 'none';
-
             ui.code.value = '';
-            let collectedData = await collectData();
-            ui.code.value = collectedData;
-
-            ui.code.style.display = 'block';
-            ui.btns.style.display = 'flex';
-            console.log('tm done'); // leave here for debugging.
-        } catch (error) {
-            ui.btns.style.display = 'none';
-            showResult(false);
-            console.log('tm error', error);
-        }
-    }, false);
-
-    ui.btncopy?.addEventListener('click', () => {
-        copyToClipboard();
-    }, false);
-
-    ui.btnclose?.addEventListener('click', () => {
-        ui.btns.style.display = 'none';
-        ui.code.style.display = 'none';
-        ui.code.value = '';
-    }, false);
-
-    document.body.insertBefore(container, document.body.firstElementChild);
+        }, false);
+    }
 }
 
 main();
